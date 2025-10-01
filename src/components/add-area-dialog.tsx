47b9b1e64f86c99293b806a03614259d6e4710b2
paulo vinicius <paulovinicius368@gmail.com>
2025-10-01 02:12:03 +0000

    Try fixing this error: `Console Error: An async function with useActionS

--- src/components/add-area-dialog.tsx
+++ src/components/add-area-dialog.tsx
@@ -104,7 +104,7 @@ export function AddAreaDialog({ children, area }: AddAreaDialogProps) {
           </DialogDescription>
         </DialogHeader>
         <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
+          <form action={formAction} onSubmit={form.handleSubmit(onSubmit)} className="space-y-4">
             <FormField
               control={form.control}
               name="sectorLote"
